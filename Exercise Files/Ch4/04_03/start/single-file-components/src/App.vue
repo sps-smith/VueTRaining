df94e9baa45aa8fe1edf93e0d19c947f19410a87
Phil Smith <phil.smith@volarisgroup.com>
2017-09-29 12:43:17 -0400

    Initial Commit

---
+++ Exercise Files/Ch4/04_03/start/single-file-components/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+      <product-list :products="theProducts" title="Shop our award-winning product line"></product-list>
+</template>
+
+<script>
+import ProductList from "./ProductList.vue";
+
+export default {
+  name: 'app',
+  data() {
+    return{
+      theProducts: []
+    };
+  },
+  components: {
+    "product-list": ProductList
+  },
+  created: function() {
+      $.getJSON('https://hplussport.com/api/products')
+          .done( data => {this.theProducts = data
+               
+          });
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ Exercise Files/Ch4/04_06/start/single-file-components/src/Product.vue
@@ -0,0 +1,27 @@
+<template>
+    <div :product="product">
+        <img :src="product.image">
+        <div class="prd">
+            <p><strong>{{product.name}}</strong></p>
+            <p>{{product.description}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'product',
+        props: ['product']
+    };
+</script>
+
+<style scoped>
+    img {
+        float: left;
+        width: 300px;
+    }
+    .prd::after {
+        clear: both;
+    }
+</style>     
+
